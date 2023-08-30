35fd532b112a033ceb9dfb019ed3a85cf94fd492
kimjaehoon <kjh950805@naver.com>
2023-08-30 11:18:08 +0900

    first

---
+++ src/app/components/footer/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <div style={{width:'80%', margin:'auto', padding:'50px 0px', textAlign:'center', backgroundColor:'lightcyan'}}>
+            Copyright &copy; 2023 한국ICT인재개발원
+        </div>
+    );
+}
